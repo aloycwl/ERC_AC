0d5a6ca0fc6c3f3b4a36047ab81432f0e07a8af4
Aloysius Chan <aloycwl@gmail.com>
2022-04-19 15:41:54 +0800

    Update ERC721AC.sol

--- ERC721AC/ERC721AC.sol
+++ ERC721AC/ERC721AC.sol
@@ -1,4 +1,4 @@
-pragma solidity^0.8.13;//SPDX-License-Identifier: MIT
+pragma solidity^0.8.13;//SPDX-License-Identifier:MIT
 interface IERC721{
     event Transfer(address indexed from,address indexed to,uint256 indexed tokenId);
     event Approval(address indexed owner,address indexed approved,uint256 indexed tokenId);
