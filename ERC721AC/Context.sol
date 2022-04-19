73385bbe1835d7eee89bded2970cadd6d37f7b31
Aloysius Chan <aloycwl@gmail.com>
2022-04-19 15:43:03 +0800

    Update Context.sol

--- ERC721AC/Context.sol
+++ ERC721AC/Context.sol
@@ -1,4 +1,4 @@
-pragma solidity^0.8.0;//SPDX-License-Identifier: MIT
+pragma solidity^0.8.13;//SPDX-License-Identifier:MIT
 abstract contract Context{
     function _msgSender()internal view virtual returns(address){
         return msg.sender;
