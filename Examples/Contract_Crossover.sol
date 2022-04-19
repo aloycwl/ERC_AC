191d9e8983996ecc63b2414b40e7597f327622d6
Aloysius Chan <aloycwl@gmail.com>
2022-04-19 15:48:14 +0800

    Create Contract_Crossover.sol

---
+++ Examples/Contract_Crossover.sol
@@ -0,0 +1,15 @@
+pragma solidity^0.8.13;//SPDX-License-Identifier:None
+
+interface IOwlContract{
+    function MINT(address _t,uint256 _a)external;
+} 
+
+contract testCrossContract{
+    IOwlContract private ioc;
+    function testMint(uint256 _t)external{
+        ioc.MINT(msg.sender,_t);
+    }
+    function setAddress(address _a)external{
+        ioc=IOwlContract(_a);
+    }
+}
