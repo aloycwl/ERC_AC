f26b9d7e2d862bcf20ab6a78294b0397141ee666
Aloysius Chan <aloycwl@gmail.com>
2022-04-19 15:40:10 +0800

    Update IERC721Receiver.sol

--- ERC721AC/IERC721Receiver.sol
+++ ERC721AC/IERC721Receiver.sol
@@ -1,4 +1,4 @@
-pragma solidity^0.8.0;//SPDX-License-Identifier: MIT
+pragma solidity^0.8.13;//SPDX-License-Identifier: MIT
 interface IERC721Receiver{
     function onERC721Received(address operator,address from,uint256 tokenId,bytes calldata data)external returns(bytes4);
 }
