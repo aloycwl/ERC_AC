094450905d314ecc21a61be1e171a39fe60e45f8
Aloysius Chan <aloycwl@gmail.com>
2022-04-19 15:37:52 +0800

    Create IERC721Receiver.sol

---
+++ ERC721AC/IERC721Receiver.sol
@@ -0,0 +1,4 @@
+pragma solidity^0.8.0;//SPDX-License-Identifier: MIT
+interface IERC721Receiver{
+    function onERC721Received(address operator,address from,uint256 tokenId,bytes calldata data)external returns(bytes4);
+}
