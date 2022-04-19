6ca0a0e90dcca94ffcedcda34de8d4bd1fcfeacb
Aloysius Chan <aloycwl@gmail.com>
2022-04-19 15:40:35 +0800

    Update IERC721Enumerable.sol

--- ERC721AC/IERC721Enumerable.sol
+++ ERC721AC/IERC721Enumerable.sol
@@ -1,4 +1,4 @@
-pragma solidity^0.8.0;//SPDX-License-Identifier: MIT
+pragma solidity^0.8.13;//SPDX-License-Identifier: MIT
 import"../IERC721.sol";
 interface IERC721Enumerable is IERC721 {
     function totalSupply()external view returns(uint256);
