7c2079cbf974390ccbc9ab5f6197fd1a94dafcfc
Aloysius Chan <aloycwl@gmail.com>
2022-04-19 15:40:45 +0800

    Update IERC721.sol

--- ERC721AC/IERC721.sol
+++ ERC721AC/IERC721.sol
@@ -1,4 +1,4 @@
-pragma solidity^0.8.0;//SPDX-License-Identifier: MIT
+pragma solidity^0.8.13;//SPDX-License-Identifier: MIT
 import "IERC165.sol";
 interface IERC721 is IERC165{
     event Transfer(address indexed from,address indexed to,uint256 indexed tokenId);
