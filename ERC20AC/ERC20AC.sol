7bff9be179d85fdb58e2bf5f016d54b1299f4f91
Aloysius Chan <aloycwl@gmail.com>
2022-05-15 12:32:09 +0800

    Update ERC20AC.sol

--- ERC20AC/ERC20AC.sol
+++ ERC20AC/ERC20AC.sol
@@ -1,4 +1,4 @@
-pragma solidity^0.8.13;//SPDX-License-Identifier:None
+pragma solidity>0.8.0;//SPDX-License-Identifier:None
 contract ERC20AC{
     event Transfer(address indexed a,address indexed b,uint c);
     event Approval(address indexed a,address indexed b,uint c);
