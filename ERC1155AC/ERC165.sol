9d34f2eda21349ea4be4b07c85ce57a001ce72aa
"Aloysius" <"aloycwl@gmail.com">
2022-05-30 10:10:48 +0800

    Created files

--- ERC1155AC/ERC165.sol
+++ ERC1155AC/ERC165.sol
@@ -1,5 +1,5 @@
-pragma solidity>0.8.0;// SPDX-License-Identifier:MIT
-import "IERC165.sol";
+pragma solidity>0.8.0;//SPDX-License-Identifier:None
+import "./IERC165.sol";
 abstract contract ERC165 is IERC165{
     function supportsInterface(bytes4 interfaceId)public view virtual override returns(bool){
         return interfaceId==type(IERC165).interfaceId;
