73f99ff8aaa15419c5e2c6808c7e639d07f7fa91
"Aloysius" <"aloycwl@gmail.com">
2022-05-30 12:10:33 +0800

    Tidying

--- ERC1155AC/ERC1155AC.sol
+++ ERC1155AC/ERC1155AC.sol
@@ -1,12 +1,5 @@
 pragma solidity>0.8.0;//SPDX-License-Identifier:None
-interface IERC165{
-    function supportsInterface(bytes4 interfaceId)external view returns(bool);
-}
-interface IERC1155Receiver is IERC165{
-    function onERC1155Received(address,address,uint256,uint256,bytes calldata) external returns(bytes4);
-    function onERC1155BatchReceived(address,address,uint256[]calldata,uint256[]calldata,bytes calldata)external returns(bytes4);
-}
-interface IERC1155 is IERC165 {
+interface IERC1155{
     event TransferSingle(address indexed operator,address indexed from,address indexed to,uint256 id,uint256 value);
     event TransferBatch(address indexed operator,address indexed from,address indexed to,uint256[]ids,uint256[]values);
     event ApprovalForAll(address indexed account,address indexed operator,bool approved);
@@ -21,203 +14,83 @@ interface IERC1155 is IERC165 {
 interface IERC1155MetadataURI is IERC1155{
     function uri(uint256 id)external view returns(string memory);
 }
-library Address{
-    function isContract(address account)internal view returns(bool){
-        return account.code.length>0;
-    }
-    function sendValue(address payable recipient,uint256 amount) internal{
-        require(address(this).balance>=amount,"Address: insufficient balance");
-        (bool success,)=recipient.call{value: amount}("");
-        require(success,"Address: unable to send value, recipient may have reverted");
-    }
-    function functionCall(address target,bytes memory data)internal returns(bytes memory){
-        return functionCall(target,data,"Address: low-level call failed");
-    }
-    function functionCall(address target,bytes memory data,string memory errorMessage)internal returns(bytes memory){
-        return functionCallWithValue(target,data,0,errorMessage);
-    }
-    function functionCallWithValue(address target,bytes memory data,uint256 value)internal returns(bytes memory){
-        return functionCallWithValue(target,data,value,"Address: low-level call with value failed");
-    }
-    function functionCallWithValue(address target,bytes memory data,uint256 value,string memory errorMessage)internal returns(bytes memory){
-        require(address(this).balance>=value,"Address: insufficient balance for call");
-        require(isContract(target),"Address: call to non-contract");
-        (bool success,bytes memory returndata)=target.call{value: value}(data);
-        return verifyCallResult(success,returndata,errorMessage);
-    }
-    function functionStaticCall(address target,bytes memory data) internal view returns(bytes memory){
-        return functionStaticCall(target,data,"Address: low-level static call failed");
-    }
-    function functionStaticCall(address target,bytes memory data,string memory errorMessage)internal view returns (bytes memory){
-        require(isContract(target),"Address: static call to non-contract");
-        (bool success,bytes memory returndata)=target.staticcall(data);
-        return verifyCallResult(success,returndata,errorMessage);
-    }
-    function functionDelegateCall(address target,bytes memory data)internal returns(bytes memory){
-        return functionDelegateCall(target,data,"Address: low-level delegate call failed");
-    }
-    function functionDelegateCall(address target,bytes memory data,string memory errorMessage)internal returns(bytes memory){
-        require(isContract(target), "Address: delegate call to non-contract");
-        (bool success,bytes memory returndata)=target.delegatecall(data);
-        return verifyCallResult(success,returndata,errorMessage);
-    }
-    function verifyCallResult(bool success,bytes memory returndata,string memory errorMessage)internal pure returns(bytes memory){
-        if(success)return returndata;
-        else{
-            if(returndata.length>0){
-                assembly{let returndata_size:=mload(returndata)revert(add(32,returndata),returndata_size)}
-            }else revert(errorMessage);
-        }
-    }
-}
-abstract contract Context{
-    function _msgSender()internal view virtual returns(address){
-        return msg.sender;
-    }
-    function _msgData()internal view virtual returns(bytes calldata){
-        return msg.data;
-    }
-}
-abstract contract ERC165 is IERC165{
-    function supportsInterface(bytes4 interfaceId)public view virtual override returns(bool){
-        return interfaceId==type(IERC165).interfaceId;
-    }
-}
-contract ERC1155 is Context,ERC165,IERC1155,IERC1155MetadataURI{
-    using Address for address;
+contract ERC1155 is IERC1155,IERC1155MetadataURI{
     mapping(uint256=>mapping(address=>uint256))private _balances;
     mapping(address=>mapping(address=>bool))private _operatorApprovals;
     string private _uri;
     constructor(string memory uri_){
-        _setURI(uri_);
+        _uri=uri_;
     }
-    function supportsInterface(bytes4 i)public view virtual override(ERC165,IERC165)returns(bool){
-        return i==type(IERC1155).interfaceId||i==type(IERC1155MetadataURI).interfaceId||super.supportsInterface(i);
+    function supportsInterface(bytes4 a)external pure returns(bool){
+        return a==type(IERC1155).interfaceId||a==type(IERC1155MetadataURI).interfaceId;
     }
-    function uri(uint256)public view virtual override returns(string memory){
+    function uri(uint256)external view virtual override returns(string memory){
         return _uri;
     }
-    function balanceOf(address account,uint256 id)public view virtual override returns(uint256){
-        require(account!=address(0));
-        return _balances[id][account];
+    function balanceOf(address a,uint256 b)public view virtual override returns(uint256){
+        return _balances[b][a];
     }
-    function balanceOfBatch(address[]memory accounts,uint256[]memory ids)public view virtual override returns(uint256[]memory){
-        require(accounts.length==ids.length);
-        uint256[]memory batchBalances=new uint256[](accounts.length);
-        for(uint256 i=0;i<accounts.length;++i)batchBalances[i]=balanceOf(accounts[i],ids[i]);
-        return batchBalances;
+    function balanceOfBatch(address[]memory a,uint256[]memory b)external view virtual override returns(uint256[]memory c){
+        c=new uint256[](a.length);
+        for(uint256 i=0;i<a.length;++i)c[i]=balanceOf(a[i],b[i]);
     }
-    function setApprovalForAll(address operator,bool approved)public virtual override{
-        _setApprovalForAll(_msgSender(),operator,approved);
+    function setApprovalForAll(address a,bool b)external virtual override{
+        _setApprovalForAll(msg.sender,a,b);
     }
-    function isApprovedForAll(address account,address operator)public view virtual override returns(bool){
-        return _operatorApprovals[account][operator];
+    function isApprovedForAll(address a,address b)public view virtual override returns(bool){
+        return _operatorApprovals[a][b];
     }
-    function safeTransferFrom(address from,address to,uint256 id,uint256 amount,bytes memory data)public virtual override{
-        require(from==_msgSender()|| isApprovedForAll(from,_msgSender()));
-        _safeTransferFrom(from,to,id,amount,data);
+    function safeTransferFrom(address a,address b,uint256 c,uint256 d,bytes memory data)public virtual override{
+        require(a==msg.sender||isApprovedForAll(a,msg.sender));
+        _safeTransferFrom(a,b,c,d,data);
     }
     function safeBatchTransferFrom(address from,address to,uint256[]memory ids,uint256[]memory amounts,bytes memory data)public virtual override{
-        require(from==_msgSender()|| isApprovedForAll(from,_msgSender()));
+        require(from==msg.sender||isApprovedForAll(from,msg.sender));
         _safeBatchTransferFrom(from,to,ids,amounts,data);
     }
-    function _safeTransferFrom(address from,address to,uint256 id,uint256 amount,bytes memory data)internal virtual{unchecked{
+    function _safeTransferFrom(address from,address to,uint256 id,uint256 amount,bytes memory)internal virtual{unchecked{
         require(to!=address(0));
-       (uint256[]memory ids,uint256[]memory amounts)=(_asSingletonArray(id),_asSingletonArray(amount));
-        _beforeTokenTransfer(msg.sender,from,to,ids,amounts,data);
         require(_balances[id][from]>=amount);
        (_balances[id][from]=_balances[id][from]-amount,_balances[id][to]+=amount);
         emit TransferSingle(msg.sender,from,to,id,amount);
-        _afterTokenTransfer(msg.sender,from,to,ids,amounts,data);
-        _doSafeTransferAcceptanceCheck(msg.sender,from,to,id,amount,data);
     }}
-    function _safeBatchTransferFrom(address from,address to,uint256[]memory ids,uint256[]memory amounts,bytes memory data)internal virtual{unchecked{
+    function _safeBatchTransferFrom(address from,address to,uint256[]memory ids,uint256[]memory amounts,bytes memory)internal virtual{unchecked{
         require(ids.length==amounts.length);
         require(to!=address(0));
-        _beforeTokenTransfer(msg.sender,from,to,ids,amounts,data);
         for(uint256 i=0;i<ids.length;++i){
             require(_balances[ids[i]][from]>=amounts[i]);
            (_balances[ids[i]][from]=_balances[ids[i]][from]-amounts[i],_balances[ids[i]][to]+=amounts[i]);
         }
         emit TransferBatch(msg.sender,from,to,ids,amounts);
-        _afterTokenTransfer(msg.sender,from,to,ids,amounts,data);
-        _doSafeBatchTransferAcceptanceCheck(msg.sender,from,to,ids,amounts,data);
     }}
-    function _setURI(string memory newuri)internal virtual{
-        _uri=newuri;
-    }
-    function _mint(address to,uint256 id,uint256 amount,bytes memory data)internal virtual{
-        require(to!=address(0));
-        address operator=_msgSender();
-        uint256[]memory ids=_asSingletonArray(id);
-        uint256[]memory amounts=_asSingletonArray(amount);
-        _beforeTokenTransfer(operator,address(0),to,ids,amounts,data);
+    function _mint(address to,uint256 id,uint256 amount,bytes memory)internal virtual{
         _balances[id][to]+=amount;
-        emit TransferSingle(operator,address(0),to,id,amount);
-        _afterTokenTransfer(operator,address(0),to,ids,amounts,data);
-        _doSafeTransferAcceptanceCheck(operator,address(0),to,id,amount,data);
+        emit TransferSingle(msg.sender,address(0),to,id,amount);
     }
-    function _mintBatch(address to,uint256[]memory ids,uint256[]memory amounts,bytes memory data)internal virtual{
-        require(to!=address(0));
+    function _mintBatch(address to,uint256[]memory ids,uint256[]memory amounts,bytes memory)internal virtual{
         require(ids.length==amounts.length);
-        _beforeTokenTransfer(msg.sender,address(0),to,ids,amounts,data);
         for(uint256 i=0;i<ids.length;i++)_balances[ids[i]][to]+=amounts[i];
         emit TransferBatch(msg.sender,address(0),to,ids,amounts);
-        _afterTokenTransfer(msg.sender,address(0),to,ids,amounts,data);
-        _doSafeBatchTransferAcceptanceCheck(msg.sender,address(0),to,ids,amounts,data);
     }
     function _burn(address from,uint256 id,uint256 amount)internal virtual{unchecked{
-        require(from!=address(0));
-        address operator=_msgSender();
-        uint256[]memory ids=_asSingletonArray(id);
-        uint256[]memory amounts=_asSingletonArray(amount);
-        _beforeTokenTransfer(operator,from,address(0),ids,amounts,"");
         uint256 fromBalance=_balances[id][from];
         require(fromBalance>=amount);
         _balances[id][from]=fromBalance -amount;
-        emit TransferSingle(operator,from,address(0),id,amount);
-        _afterTokenTransfer(operator,from,address(0),ids,amounts,"");
+        emit TransferSingle(msg.sender,from,address(0),id,amount);
     }}
     function _burnBatch(address from,uint256[]memory ids,uint256[]memory amounts)internal virtual{unchecked{
         require(from!=address(0));
         require(ids.length==amounts.length);
-        address operator=_msgSender();
-        _beforeTokenTransfer(operator,from,address(0),ids,amounts,"");
         for(uint256 i=0;i<ids.length;i++){
             require(_balances[ids[i]][from]>=amounts[i]);
             _balances[ids[i]][from]=_balances[ids[i]][from]-amounts[i];
         }
-        emit TransferBatch(operator,from,address(0),ids,amounts);
-        _afterTokenTransfer(operator,from,address(0),ids,amounts,"");
+        emit TransferBatch(msg.sender,from,address(0),ids,amounts);
     }}
     function _setApprovalForAll(address owner,address operator,bool approved)internal virtual{
         require(owner!=operator);
         _operatorApprovals[owner][operator]=approved;
         emit ApprovalForAll(owner,operator,approved);
-    }
-    function _beforeTokenTransfer(address operator,address from,address to,uint256[]memory ids,uint256[]memory amounts,bytes memory data)internal virtual{}
-    function _afterTokenTransfer(address operator,address from,address to,uint256[]memory ids,uint256[]memory amounts,bytes memory data)internal virtual{}
-    function _doSafeTransferAcceptanceCheck(address operator,address from,address to,uint256 id,uint256 amount,bytes memory data)private{
-        if(to.isContract())
-        try IERC1155Receiver(to).onERC1155Received(operator,from,id,amount,data)returns(bytes4 response){
-            if(response!=IERC1155Receiver.onERC1155Received.selector)revert();
-        }catch Error(string memory reason){
-            revert(reason);
-        }catch{
-            revert();
-        }
-    }
-    function _doSafeBatchTransferAcceptanceCheck(address operator,address from,address to,uint256[]memory ids,uint256[]memory amounts,bytes memory data)private{
-        if(to.isContract())
-        try IERC1155Receiver(to).onERC1155BatchReceived(operator,from,ids,amounts,data)returns(
-            bytes4 response
-        ){
-            if(response!=IERC1155Receiver.onERC1155BatchReceived.selector)revert();
-        }catch Error(string memory reason){
-            revert(reason);
-        }catch{
-            revert();
-        }
     }
     function _asSingletonArray(uint256 element)private pure returns(uint256[]memory array){
         array=new uint256[](1);
