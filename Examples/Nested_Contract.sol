bbebd6ecb842f3f3ba914f8ad85ff0dcc08cffd4
aloycwl <aloycw@gmail.com>
2023-03-06 15:01:48 +0800

    Nested contract with child storage

--- Examples/Nested_Contract.sol
+++ Examples/Nested_Contract.sol
@@ -1,19 +1,28 @@
-/*
-To view child's contract, put the returned address in "At Address"
-*/
-
 pragma solidity>0.8.0;//SPDX-License-Identifier:None
 
 contract Child{
-    uint public amount;
+    uint private amount;
     constructor(uint _amount){
         amount=_amount;
+    }
+    function amt()external view returns(uint){
+        return amount;
     }
 }
 
 contract Parent{
-    function createChild(uint _amount) public returns(Child) {
-        Child child = new Child(_amount);
-        return child;
+    mapping(uint=>address)private childs;
+    uint private count;
+    function createChild(uint _amount)external{
+        Child child=new Child(_amount);
+        childs[count]=address(child);
+        count++;
+    }
+    function displayChilds()external view returns(address[] memory _c){
+        _c=new address[](count);
+        for(uint i=0;i<count;i++)_c[i]=childs[i];
+    }
+    function fetchAmt(address _a)external view returns(uint){
+        return Child(_a).amt();
     }
 }
