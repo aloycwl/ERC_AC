a66dc8bd2bdc3c46778932310ee5c64bd5015857
aloycwl <aloycwl@gmail.com>
2023-06-29 17:09:03 +0800

    further reduce variables

--- ERC20AC/ERC20AC.sol
+++ ERC20AC/ERC20AC.sol
@@ -1,37 +1,48 @@
-pragma solidity>0.8.0;//SPDX-License-Identifier:None
+//SPDX-License-Identifier:None
+pragma solidity>0.8.0;
+
 contract ERC20AC{
-    event Transfer(address indexed from,address indexed to,uint value);
-    event Approval(address indexed owner,address indexed spender,uint value);
-    mapping(address=>mapping(address=>uint))private _allowances;
-    mapping(address=>uint)private _balances;
-    uint public constant decimals=18;
-    uint public totalSupply=1e24;
-    address private _owner;
-    string public name="Name";
-    string public symbol="SYM";
-    constructor(){
-        _owner=msg.sender;
-    }
-    function balanceOf(address a)external view returns(uint){
-        return _balances[a];
-    }
-    function transfer(address a,uint b)external returns(bool){
-        return transferFrom(msg.sender,a,b);
-    }
-    function allowance(address a,address b)external view returns(uint){
-        return _allowances[a][b];
-    }
-    function approve(address a,uint b)external returns(bool){
-        _allowances[msg.sender][a]=b;
-        emit Approval(msg.sender,a,b);
+    event Transfer (address indexed from,  address indexed to,      uint value);
+    event Approval (address indexed owner, address indexed spender, uint value);
+
+    mapping(address => mapping(address => uint)) public allowance;
+    mapping(address => uint)                     public balanceOf;
+    uint constant                                public decimals = 18;
+    uint                                         public totalSupply = 1e24;
+    string constant                              public name = "Name";
+    string constant                              public symbol = "SYM";
+
+    function approve (address a, uint b) external returns (bool) {
+        
+        emit Approval(msg.sender, a, allowance[msg.sender][a] = b);
         return true;
+
     }
-    function transferFrom(address a,address b,uint c)public virtual returns(bool){unchecked{
-        require(_balances[a]>=c);
-        require(a==msg.sender||_allowances[a][b]>=c);
-        if(_allowances[a][b]>=c)_allowances[a][b]-=c;
-        (_balances[a]-=c,_balances[b]+=c);
-        emit Transfer(a,b,c);
-        return true;
-    }}
+
+    function transfer (address a, uint b) external returns (bool) {
+
+        return transferFrom(msg.sender, a, b);
+
+    }
+    
+    function transferFrom (address a, address b, uint c) public returns (bool) {
+
+        unchecked {
+
+            uint allow = allowance[a][b];    
+
+            assert(balanceOf[a] >= c);
+            assert(a == msg.sender || allow >= c);
+
+
+            if (allow >= c) allowance[a][b] -= c;
+            (balanceOf[a] -= c, balanceOf[b] += c);
+
+            emit Transfer(a,b,c);
+            return true;
+
+        }
+
+    }
+
 }
