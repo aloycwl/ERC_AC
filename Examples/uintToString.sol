eadc0ea5cd39b11c30662e2d0a87f5aab20355bf
aloycwl <aloycwl@gmail.com>
2023-06-30 14:18:44 +0800

    Updated pragma

--- Examples/uintToString.sol
+++ Examples/uintToString.sol
@@ -1,14 +1,24 @@
-pragma solidity>0.8.0;//SPDX-License-Identifier:None
+//SPDX-License-Identifier:None
+pragma solidity ^0.8.18;
+pragma abicoder v1;
 
-contract StringUtil{
+contract StringUtil {
+
     function u2s(uint a) public pure returns (string memory) {
-        if (a == 0) return "0";
-        uint j = a;
-        uint l;
-        while (j != 0) (++l, j /= 10);
-        bytes memory bstr = new bytes(l);
-        j = a;
-        while (j != 0) (bstr[--l] = bytes1(uint8(48 + j % 10)), j /= 10);
-        return string(bstr);
+
+        unchecked {
+        
+            if (a == 0) return "0";
+            uint j = a;
+            uint l;
+            while (j != 0) (++l, j /= 10);
+            bytes memory bstr = new bytes(l);
+            j = a;
+            while (j != 0) (bstr[--l] = bytes1(uint8(48 + j % 10)), j /= 10);
+            return string(bstr);
+            
+        }
+
     }
+
 }
