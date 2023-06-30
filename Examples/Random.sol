c9093ec386401aaf5d3585f3b405927474633667
aloycwl <aloycwl@gmail.com>
2023-06-30 14:17:54 +0800

    Updated pragma

--- Examples/Random.sol
+++ Examples/Random.sol
@@ -1,11 +1,29 @@
-pragma solidity>0.8.0;//SPDX-License-Identifier:None
+//SPDX-License-Identifier:None
+pragma solidity ^0.8.18;
+pragma abicoder v1;
 
 contract random{
-    function ranArr()external view returns(uint[10] memory array){unchecked{
-        uint something=uint(keccak256(abi.encodePacked(block.timestamp)));
-        for(uint i=0;i<10;i++)(array[i]=something%2,something/=block.difficulty);
-    }}
-    function ranNum()external view returns(uint){unchecked{
-        return uint(keccak256(abi.encodePacked(block.timestamp)))%10;
-    }}
+
+    function ranArr () external view returns (uint[10] memory array) {
+        
+        unchecked{
+
+            uint something=uint(keccak256(abi.encodePacked(block.timestamp)));
+
+            for(uint i; i < 10; ++i) (array[i] = something % 2, something /= block.difficulty);
+
+        }
+
+    }
+
+    function ranNum () external view returns (uint) {
+
+        unchecked {
+
+            return uint(keccak256(abi.encodePacked(block.timestamp))) % 10;
+
+        }
+
+    }
+    
 }
