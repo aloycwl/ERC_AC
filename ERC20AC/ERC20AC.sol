dcd4d91e4bed89c533260d69b6a2d9e388b672fa
aloycwl <aloycwl@gmail.com>
2023-07-06 10:54:55 +0800

    Formatted

--- ERC20AC/ERC20AC.sol
+++ ERC20AC/ERC20AC.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.18;
 pragma abicoder v1;
 
-
 contract ERC20AC{
-    event Transfer (address indexed from,  address indexed to,      uint value);
-    event Approval (address indexed owner, address indexed spender, uint value);
+
+    event Transfer(address indexed from,  address indexed to,      uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
 
     mapping(address => mapping(address => uint)) public allowance;
     mapping(address => uint)                     public balanceOf;
@@ -14,16 +14,16 @@ contract ERC20AC{
     string constant                              public name        = "Name";
     string constant                              public symbol      = "SYM";
 
-    function approve (address a, uint b) external returns (bool) {
+    function approve (address to, uint amt) external returns(bool) {
         
-        emit Approval(msg.sender, a, allowance[msg.sender][a] = b);
+        emit Approval(msg.sender, to, allowance[msg.sender][to] = amt);
         return true;
 
     }
 
-    function transfer (address a, uint b) external returns (bool) {
+    function transfer (address to, uint amt) external returns(bool) {
 
-        return transferFrom(msg.sender, a, b);
+        return transferFrom(msg.sender, to, amt);
 
     }
     
@@ -33,11 +33,9 @@ contract ERC20AC{
 
             uint allow = allowance[a][b];    
 
-            assert(balanceOf[a] >= c);
-            assert(a == msg.sender || allow >= c);
+            assert(balanceOf[a] >= c && (a == msg.sender || allow >= c));
 
-
-            if (allow >= c) allowance[a][b] -= c;
+            if(allow >= c) allowance[a][b] -= c;
             (balanceOf[a] -= c, balanceOf[b] += c);
 
             emit Transfer(a, b, c);
