47a09e88c8e25b07db251dcb982f7133cf920ac8
aloycwl <aloycwl@gmail.com>
2023-08-10 14:53:08 +0800

    Done ERC20 Assembly

--- ERC20AC/ERC20AC.sol
+++ ERC20AC/ERC20AC.sol
@@ -4,45 +4,110 @@ pragma abicoder v1;
 
 contract ERC20AC{
 
-    event Transfer(address indexed from,  address indexed to,      uint value);
-    event Approval(address indexed owner, address indexed spender, uint value);
+    event Transfer(address indexed from, address indexed to, uint);
+    event Approval(address indexed owner, address indexed spender, uint);
+    bytes32 constant private TTS = 0x37a831c0f5b7923496e8c771dd27d3c61d195f75312929773a24c59604283ed2;
+    bytes32 constant private APP = 0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925;
+    bytes32 constant private TTF = 0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef;
 
-    mapping(address => mapping(address => uint)) public allowance;
-    mapping(address => uint)                     public balanceOf;
-    uint constant                                public decimals    = 18;
-    uint                                         public totalSupply = 1e24;
-    string constant                              public name        = "Name";
-    string constant                              public symbol      = "SYM";
-
-    function approve (address to, uint amt) external returns(bool) {
-        
-        emit Approval(msg.sender, to, allowance[msg.sender][to] = amt);
-        return true;
-
+    constructor() {
+        assembly {
+            sstore(caller(), 0x084595161401484a000000)
+        }
     }
 
-    function transfer (address to, uint amt) external returns(bool) {
+    function name() external pure returns(string memory) {
+        assembly {
+            mstore(0x80, 0x20)
+            mstore(0xa0, 0x04)
+            mstore(0xc0, "Name")
+            return(0x80, 0x60)
+        }
+    }
 
-        return transferFrom(msg.sender, to, amt);
+    function symbol() external pure returns(string memory) {
+        assembly {
+            mstore(0x80, 0x20)
+            mstore(0xa0, 0x03)
+            mstore(0xc0, "SYM")
+            return(0x80, 0x60)
+        }
+    }
 
+    function decimals() external pure returns(uint) {
+        assembly {
+            mstore(0x00, 0x12)
+            return(0x00, 0x20)
+        }
+    }
+
+    function allowance(address adr, address ad2) external view returns(uint) {
+        assembly {
+            mstore(0x00, adr)
+            mstore(0x20, ad2)
+            mstore(0x00, sload(keccak256(0x00, 0x40)))
+            return(0x00, 0x20)
+        }
+    }
+
+    function balanceOf(address adr) external view returns(uint) {
+        assembly {
+            mstore(0x00, sload(adr))
+            return(0x00, 0x20)
+        }
+    }
+
+    function totalSupply() external view returns(uint) {
+        assembly {
+            mstore(0x00, sload(TTS))
+            return(0x00, 0x20)
+        }
+    }
+
+    function approve(address adr, uint amt) external returns(bool) {
+        assembly {
+            mstore(0x00, caller())
+            mstore(0x20, adr)
+            sstore(keccak256(0x00, 0x40), amt)
+            mstore(0x00, amt)
+            log3(0x00, 0x20, APP, caller(), adr)
+            mstore(0x00, 0x01)
+            return(0x00, 0x20)
+        }
+    }
+
+    function transfer(address adr, uint amt) external returns(bool) {
+        assembly {
+            let bal := sload(caller())
+            if gt(amt, bal) {
+                revert(0x00, 0x00)
+            }
+            sstore(caller(), sub(bal, amt))
+            sstore(adr, add(sload(adr), amt))
+            mstore(0x00, amt)
+            log3(0x00, 0x20, TTF, caller(), adr)
+            mstore(0x00, 0x01)
+            return(0x00, 0x20)
+        }
     }
     
-    function transferFrom (address a, address b, uint c) public returns (bool) {
-
-        unchecked {
-
-            uint allow = allowance[a][b];    
-
-            assert(balanceOf[a] >= c && (a == msg.sender || allow >= c));
-
-            if(allow >= c) allowance[a][b] -= c;
-            (balanceOf[a] -= c, balanceOf[b] += c);
-
-            emit Transfer(a, b, c);
-            return true;
-
+    function transferFrom(address adr, address ad2, uint amt) public returns (bool) {
+        assembly {
+            let bal := sload(adr)
+            mstore(0x00, adr)
+            mstore(0x20, ad2)
+            let ptr := keccak256(0x00, 0x40)
+            let alw := sload(ptr)
+            if or(gt(amt, bal), gt(amt, alw)) {
+                revert(0x00, 0x00)
+            }
+            sstore(ptr, sub(alw, amt))
+            sstore(adr, sub(bal, amt))
+            sstore(ad2, add(sload(ad2), amt))
+            mstore(0x00, amt)
+            log3(0x00, 0x20, TTF, caller(), adr)
+            mstore(0x00, 0x01)
+            return(0x00, 0x20)
         }
-
     }
-
 }
--- README.md
+++ README.md
@@ -2,17 +2,18 @@
 
 Ethereum Request for Command AC Version
 
+All is written in Assembly and Yul for gas saving
+
 ERC721AC
 
 ERC20AC
 
 ERC1155AC
 
-ERC1967AC - full contract in assembly
+ERC1967AC
 
 Util/OnlyAccess<br>
 <i>This function gives ownership and access to more than 1 address<br>
 E.g. When multiple contracts are burning and minting an ERC20 token,<br>
 This script can enact or revoke the access for anyone with access</i>
 
-Will be converting all the ERC standards to full assembly and Yul for gas savings
